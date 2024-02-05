7397b38cd4b3bc0037212120cc7bc6d022814a99
Flávio José <76021739+Flaviojsouza@users.noreply.github.com>
2024-02-05 11:57:09 -0300

    js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let duvidas = document.querySelectorAll('.duvida');
+
+duvidas.forEach(function(duvida) {
+    duvida.addEventListener("click", function (){
+        duvida.classList.toggle("ativa")
+    });
+});
